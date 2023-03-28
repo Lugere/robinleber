978cf3c6fb151abeb59cc03f778f3260ef8b0ce4
Robin Leber <leber.beselich@web.de>
2023-03-28 13:32:52 +0200

    updated profile image, moved socialmedia

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
     <div id="app">
         <header>
-            <router-link to="Home" class="name">Robin Leber</router-link>
+            <!-- <router-link to="Home" class="name">Robin Leber</router-link> 
             <nav>
                 <div class="pattern"></div>
                 <ul class="links">
                     <li class="link">
-                        <a href=""> Home </a>
+                        <a rel="noreferrer noopener" href=""> Home </a>
                     </li>
                     <li class="link">
-                        <a href=""> Projekte </a>
+                        <a rel="noreferrer noopener" href=""> Projekte </a>
                     </li>
                     <li class="link">
-                        <a href=""> Über mich </a>
+                        <a rel="noreferrer noopener" href=""> Über mich </a>
                     </li>
                 </ul>
-            </nav>
+            </nav> -->
         </header>
         <router-view class="app-body" />
+        <footer>
+            ©2023 Robin Leber
+        </footer>
     </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,26 +3,7 @@
         <div class="profile">
             <div class="picture">
                 <div class="face">
-                    <img src="@/assets/image/rl_face.jpg" alt="" />
-                </div>
-                <div class="sm-container">
-                    <a
-                        href="https://www.xing.com/profile/Robin_Leber2/cv"
-                        target="_blank"
-                        class="sm sm-xing"
-                    >
-                        <i class="fab fa-xing" />
-                    </a>
-                    <a href="https://github.com/Lugere" target="_blank" class="sm sm-github">
-                        <i class="fab fa-github" />
-                    </a>
-                    <a
-                        href="https://www.linkedin.com/in/robin-leber-b14987221/"
-                        target="_blank"
-                        class="sm sm-linked-in"
-                    >
-                        <i class="fab fa-linkedin-in" />
-                    </a>
+                    <img src="@/assets/image/rl_face2.png" alt="" />
                 </div>
             </div>
             <div class="text">
@@ -38,14 +19,128 @@
                     </div>
                 </div>
                 <div class="job">
-                    Web Developer
+                    Junior Web Developer
+                </div>
+            </div>
+            <div class="social-media">
+                <a
+                    rel="noreferrer noopener"
+                    href="https://www.xing.com/profile/Robin_Leber2/cv"
+                    target="_blank"
+                    class="sm sm-xing"
+                >
+                    <i class="fab fa-xing" />
+                </a>
+                <a rel="noreferrer noopener" href="https://github.com/Lugere" target="_blank" class="sm sm-github">
+                    <i class="fab fa-github" />
+                </a>
+                <a
+                    rel="noreferrer noopener"
+                    href="https://www.linkedin.com/in/robin-leber-b14987221/"
+                    target="_blank"
+                    class="sm sm-linked-in"
+                >
+                    <i class="fab fa-linkedin-in" />
+                </a>
+            </div>
+        </div>
+        <!-- <div data-name="" class="follow-me-header">
+            Social
+        </div>
+        <div class="follow-me">
+            <a
+                rel="noreferrer noopener"
+                href="https://www.xing.com/profile/Robin_Leber2/cv"
+                target="_blank"
+                class="sm sm-xing"
+            >
+                <i class="fab fa-xing" />
+            </a>
+            <a rel="noreferrer noopener" href="https://github.com/Lugere" target="_blank" class="sm sm-github">
+                <i class="fab fa-github" />
+            </a>
+            <a
+                rel="noreferrer noopener"
+                href="https://www.linkedin.com/in/robin-leber-b14987221/"
+                target="_blank"
+                class="sm sm-linked-in"
+            >
+                <i class="fab fa-linkedin-in" />
+            </a>
+        </div> -->
+        <div class="project-header" data-name="">Meine Projekte</div>
+        <div class="projects">
+            <div class="project la-luna">
+                <div class="image">
+                    <img src="@/assets/image/projects/la-luna.png" alt="" />
+                </div>
+                <div class="content">
+                    <div class="title">Ristorante La Luna</div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est cupiditate molestias, dolorem
+                        repudiandae, enim earum aliquam placeat corporis recusandae animi vero illum laudantium hic
+                        deleniti optio. Ipsum dolore vitae aliquid.
+                    </div>
+                    <div class="links">
+                        <a
+                            rel="noreferrer noopener"
+                            class="github"
+                            role="button"
+                            href="https://github.com/Lugere/la-luna"
+                            target="_blank"
+                        >
+                            <i class="fab fa-github" />
+                            Zum Github Projekt
+                        </a>
+                        <a
+                            rel="noreferrer noopener"
+                            class="web"
+                            role="button"
+                            href="https://www.ristorante-la-luna.de/"
+                            target="_blank"
+                        >
+                            <i class="fas fa-globe" />
+                            Zur Website
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="project coaching-airline">
+                <div class="image">
+                    <img src="@/assets/image/projects/coaching-airline.png" alt="" />
+                </div>
+                <div class="content">
+                    <div class="title">Ristorante La Luna</div>
+                    <div class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est cupiditate molestias, dolorem
+                        repudiandae, enim earum aliquam placeat corporis recusandae animi vero illum laudantium hic
+                        deleniti optio. Ipsum dolore vitae aliquid.
+                    </div>
+                    <div class="links">
+                        <a
+                            rel="noreferrer noopener"
+                            class="github"
+                            role="button"
+                            href="https://github.com/Lugere/la-luna"
+                            target="_blank"
+                        >
+                            <i class="fab fa-github" />
+                            Github Projekt
+                        </a>
+                        <a
+                            rel="noreferrer noopener"
+                            class="web"
+                            role="button"
+                            href="https://www.coachingairlineradio.de/Home"
+                            target="_blank"
+                        >
+                            <i class="fas fa-globe" />
+                            Website
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="under-construction">
-            website still under construction!
-        </div>
-        <p class="home-body"></p>
     </div>
 </template>
 
